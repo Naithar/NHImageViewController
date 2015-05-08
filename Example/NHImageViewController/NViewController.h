85c419f580003172c66fd8412b99526d2937b75f
Naithar <devias.naith@gmail.com>
2015-05-08 14:30:32 +0300

    Initial commit

---
+++ Example/NHImageViewController/NViewController.h
@@ -0,0 +1,13 @@
+//
+//  NViewController.h
+//  NHImageViewController
+//
+//  Created by Naithar on 05/08/2015.
+//  Copyright (c) 2014 Naithar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NViewController : UIViewController
+
+@end
---
+++ Example/NHImageViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NHImageViewController
+//
+//  Created by Naithar on 05/08/2015.
+//  Copyright (c) 2014 Naithar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NAppDelegate class]));
+    }
+}
